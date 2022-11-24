2934b1c709eb627f83a2d3ef871ee5510b48d4fc
DonCosme <109149406+franema@users.noreply.github.com>
2022-11-24 19:27:20 -0300

    Binds events to inputs

--- src/Components/CV-Inputs/GeneralInfo.js
+++ src/Components/CV-Inputs/GeneralInfo.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
-import "../../styles.css"
+
 
 class GeneralInfo extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+
   render() {
     return (
       <div className='generalInfo-container'>
-          <input type="text" className='generalInfo-input' id='firstName' placeholder='First Name'></input>
-          <input type="text" className='generalInfo-input' id='lastName' placeholder='Last Name'></input>
-          <input type="text" className='generalInfo-input' id='title' placeholder='Title'></input>
-          <input type="text" className='generalInfo-input' id='adress' placeholder='Adress'></input>
-          <input type="text" className='generalInfo-input' id='phone' placeholder='Phone'></input>
-          <input type="text" className='generalInfo-input' id='email' placeholder='Email'></input>
-          <input type="text" className='generalInfo-input' id='description' placeholder='Description'></input>
+          <input type="text" className='generalInfo-input' id='firstName' placeholder='First Name' onChange={this.props.handleChange}></input>
+          <input type="text" className='generalInfo-input' id='lastName' placeholder='Last Name' onChange={this.props.handleChange}></input>
+          <input type="text" className='generalInfo-input' id='title' placeholder='Title' onChange={this.props.handleChange}></input>
+          <input type="text" className='generalInfo-input' id='adress' placeholder='Adress' onChange={this.props.handleChange}></input>
+          <input type="text" className='generalInfo-input' id='phone' placeholder='Phone' onChange={this.props.handleChange}></input>
+          <input type="text" className='generalInfo-input' id='email' placeholder='Email' onChange={this.props.handleChange}></input>
+          <input type="text" className='generalInfo-input' id='description' placeholder='Description' onChange={this.props.handleChange}></input>
       </div>
     )
   }
