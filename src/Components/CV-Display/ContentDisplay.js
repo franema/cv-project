deb801fa84bebe086f9aec1143d3e846265d2941
DonCosme <109149406+franema@users.noreply.github.com>
2022-11-25 10:25:13 -0300

    Rearrange cv layout

--- src/Components/CV-Display/ContentDisplay.js
+++ src/Components/CV-Display/ContentDisplay.js
@@ -11,24 +11,46 @@ class ContentDisplay extends Component {
   }
 
   render() {
-    const { edInfo } = this.props
+    const { cvInfo } = this.props
 
     return (
       <div className='cv-display'>
         <div className='cv-title'>
           <div className='cv-name'>
-            <p className='info-display'>{edInfo.firstName}</p>
-            <p className='info-display'>{edInfo.lastName}</p>
+            <p className='info-display'>{cvInfo.firstName}</p>
+            <p className='info-display'>{cvInfo.lastName}</p>
           </div>
-          <p className='info-display'>{edInfo.title}</p>
+          <p className='info-display'>{cvInfo.title}</p>
         </div>
-        <div className='cv-education'>
-          <h2>Education</h2>
-          <h4 className='info-display'>{edInfo.university}</h4>
-          <p className='info-display'>{edInfo.degree}</p>
-          <p className='info-display'>{edInfo.subject}</p>
-          <p className='info-display'>{edInfo.from} - {edInfo.to}</p>
-          <p className='info-display'></p>
+        <div className='cv-content'>
+          <div className='cv-contact'>
+            <h2>Adress</h2>
+            <p className='info-display'>{cvInfo.adress}</p>
+            <h2>Phone</h2>
+            <p className='info-display'>{cvInfo.phone}</p>
+            <h2>Email</h2>
+            <p className='info-display'>{cvInfo.email}</p>
+          </div>
+          <div className='cv-main'>
+            <div className='cv-description'>
+              <h2>Description</h2>
+              <p className='info-display'>{cvInfo.description}</p>
+            </div>
+            <div className='cv-experience'>
+              <h2>Experience</h2>
+              <h4 className='info-display'>{cvInfo.position}</h4>
+              <p className='info-display'>{cvInfo.company}</p>
+              <p className='info-display'>{cvInfo.pr_from} - {cvInfo.pr_to}</p>
+            </div>
+            <div className='cv-education'>
+              <h2>Education</h2>
+              <h4 className='info-display'>{cvInfo.university}</h4>
+              <p className='info-display'>{cvInfo.degree}</p>
+              <p className='info-display'>{cvInfo.subject}</p>
+              <p className='info-display'>{cvInfo.ed_from} - {cvInfo.ed_to}</p>
+            </div>
+          </div>
+
         </div>
       </div>
     )
